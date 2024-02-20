ef7fd6886fe1b7cf45d17a6896a45f41c20164ca
Iqbal Fauzi <iqbalfawz@gmail.com>
2024-02-20 17:31:12 +0700

    First commit

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    chrome: {
+        bin: '/usr/bin/google-chrome'
+    },
+    puppeteer: {
+        user: {
+            data: '/home/iqbal/Desktop/surfer'
+        },
+        launch: {
+            headless: false
+        }
+    }
+}
